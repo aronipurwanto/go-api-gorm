4c665d519ac39d15a179053c5b6954e1562ce710
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2025-05-15 10:33:08 +0700

    feat: Category Repository

--- repositories/category_repo.go
+++ repositories/category_repo.go
@@ -22,3 +22,45 @@ func NewCategoryRepo(db *gorm.DB) CategoryRepo {
 		db: db,
 	}
 }
+
+func (c *categoryRepoImpl) GetAll() ([]models.Category, error) {
+	var categories []models.Category
+	err := c.db.Find(&categories).Error
+	if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (c *categoryRepoImpl) GetByID(id int) (models.Category, error) {
+	var category models.Category
+	err := c.db.First(&category, id).Error
+	if err != nil {
+		return category, err
+	}
+	return category, nil
+}
+
+func (c *categoryRepoImpl) Create(category models.Category) (models.Category, error) {
+	err := c.db.Create(&category).Error
+	if err != nil {
+		return category, err
+	}
+	return category, nil
+}
+
+func (c *categoryRepoImpl) Update(category models.Category) (models.Category, error) {
+	err := c.db.Save(&category).Error
+	if err != nil {
+		return category, err
+	}
+	return category, nil
+}
+
+func (c *categoryRepoImpl) Delete(id int) error {
+	err := c.db.Delete(&models.Category{}, id).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
